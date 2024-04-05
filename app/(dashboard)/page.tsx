90c62fce4244324d84e2a171fb8054882b02a51d
daemon-reconfig <mehulishi27@gmail.com>
2024-04-05 20:24:47 +0530

    Adding Convex and Clerk auth

---
+++ app/(dashboard)/page.tsx
@@ -0,0 +1,14 @@
+import { UserButton } from "@clerk/nextjs";
+
+export default function Dashboard() {
+  return (
+    <div className="flex flex-col gap-y-4">
+      <div>
+        TEsting Phahse
+      </div>
+      <div>
+        <UserButton />
+      </div>
+    </div>
+  );
+}
